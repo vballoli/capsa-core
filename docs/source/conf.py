e84aefefe421c4b86a404aaa1f3531e1cbcba125
Alexander Amini <xan.amini@gmail.com>
2023-06-19 17:00:45 -0400

    fixing rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,6 +28,8 @@ author = 'Themis AI'
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
